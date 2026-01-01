439370b4812fe5e9db2aabc4c11a3f71480d618e
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-01-01 22:21:16 +0000

    Add TEST_DB_URL to test_hooks

--- tests/integration/test_hooks.py
+++ tests/integration/test_hooks.py
@@ -7,3 +7,4 @@ from migretti.core import apply_migrations
 from migretti import __main__ as main_mod
 from migretti.logging_setup import setup_logging
 TEST_DB_NAME = "migretti_test"
+TEST_DB_URL = f"postgresql://postgres:postgres@localhost:5432/{TEST_DB_NAME}"
