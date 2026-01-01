05d71afa8b1ba5d50bd492732b8d63daaf109683
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-01-01 23:30:10 +0000

    Add TEST_DB_URL to test_smart_dry_run

--- tests/integration/test_smart_dry_run.py
+++ tests/integration/test_smart_dry_run.py
@@ -6,3 +6,4 @@ import tempfile
 from migretti import __main__ as main_mod
 from migretti.logging_setup import setup_logging
 TEST_DB_NAME = "migretti_test"
+TEST_DB_URL = f"postgresql://postgres:postgres@localhost:5432/{TEST_DB_NAME}"
