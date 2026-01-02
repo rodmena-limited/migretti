c623652f87492db543c46e19f683d7324cc16016
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-01-02 21:11:42 +0000

    Initial test_seed module structure

---
+++ tests/test_seed.py
@@ -0,0 +1,8 @@
+import pytest
+import os
+import psycopg
+from migretti.__main__ import cmd_seed
+from migretti.logging_setup import setup_logging
+import tempfile
+from migretti import __main__ as main_mod
+TEST_DB_NAME = "migretti_test"
