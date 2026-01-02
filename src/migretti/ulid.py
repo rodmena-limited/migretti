32f3e80636752ee46bca1e2e5cb769e1b9afbf3a
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-01-02 23:00:12 +0000

    Complete ulid implementation

--- src/migretti/ulid.py
+++ src/migretti/ulid.py
@@ -1,7 +1,25 @@
+# Minimal ULID implementation for migretti
+# Based on python-ulid by Martin Domke (MIT License)
+# https://github.com/mdomke/python-ulid
+#
+# Copyright (c) 2017 Martin Domke
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
 from __future__ import annotations
+
 import os
 import time
 from threading import Lock
+
+# Constants
 MILLISECS_IN_SECS = 1000
 NANOSECS_IN_MILLISECS = 1000000
 MIN_TIMESTAMP = 0
@@ -11,7 +29,10 @@ MAX_RANDOMNESS = b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
 TIMESTAMP_LEN = 6
 RANDOMNESS_LEN = 10
 BYTES_LEN = TIMESTAMP_LEN + RANDOMNESS_LEN
+
+# Base32 encoding table (Crockford's Base32)
 ENCODE = "0123456789ABCDEFGHJKMNPQRSTVWXYZ"
+
 
 def _encode_timestamp(binary: bytes) -> str:
     """Encode 6 bytes of timestamp to 10 base32 characters."""
@@ -30,6 +51,7 @@ def _encode_timestamp(binary: bytes) -> str:
             lut[(binary[5] & 31)],
         ]
     )
+
 
 def _encode_randomness(binary: bytes) -> str:
     """Encode 10 bytes of randomness to 16 base32 characters."""
@@ -55,14 +77,17 @@ def _encode_randomness(binary: bytes) -> str:
         ]
     )
 
+
 def _encode(binary: bytes) -> str:
     """Encode 16 bytes to 26 character ULID string."""
     return _encode_timestamp(binary[:TIMESTAMP_LEN]) + _encode_randomness(
         binary[TIMESTAMP_LEN:]
     )
 
+
 class _ValueProvider:
     """Thread-safe provider for timestamp and monotonic randomness."""
+
     def __init__(self) -> None:
         self.lock = Lock()
         self.prev_timestamp = MIN_TIMESTAMP
@@ -90,3 +115,38 @@ class _ValueProvider:
             self.prev_randomness = randomness
             self.prev_timestamp = current_timestamp
         return randomness
+
+    def _increment_bytes(self, value: bytes) -> bytes:
+        length = len(value)
+        return (int.from_bytes(value, byteorder="big") + 1).to_bytes(
+            length, byteorder="big"
+        )
+
+
+class ULID:
+    """
+    Universally Unique Lexicographically Sortable Identifier.
+
+    A ULID consists of a 48-bit timestamp and 80 bits of randomness,
+    encoded as a 26-character string.
+
+    Usage:
+        >>> ulid = ULID()
+        >>> str(ulid)
+        '01H5S3K8JXQY7GZWPV2N4M6R9T'
+    """
+
+    _provider = _ValueProvider()
+
+    def __init__(self) -> None:
+        timestamp_ms = self._provider.timestamp()
+        timestamp_bytes = int.to_bytes(timestamp_ms, TIMESTAMP_LEN, "big")
+        randomness_bytes = self._provider.randomness()
+        self._bytes: bytes = timestamp_bytes + randomness_bytes
+
+    def __str__(self) -> str:
+        """Encode as a 26 character base32 string."""
+        return _encode(self._bytes)
+
+    def __repr__(self) -> str:
+        return f"ULID({self!s})"
