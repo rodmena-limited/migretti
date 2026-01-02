b7f94aa881262777145f9156a014ce1566a69742
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-01-02 00:08:47 +0000

    Initial test_squash module structure

---
+++ tests/integration/test_squash.py
@@ -0,0 +1,9 @@
+import pytest
+import os
+import glob
+from migretti.__main__ import cmd_squash, cmd_create
+from migretti.logging_setup import setup_logging
+from migretti import __main__ as main_mod
+import psycopg
+import tempfile
+TEST_DB_NAME = "migretti_test"
