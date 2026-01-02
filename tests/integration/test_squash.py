13e58329d6b501651cda2b739cbd67068a2f736e
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-01-02 00:24:56 +0000

    Add test_db to test_squash

--- tests/integration/test_squash.py
+++ tests/integration/test_squash.py
@@ -8,3 +8,18 @@ import psycopg
 import tempfile
 TEST_DB_NAME = "migretti_test"
 TEST_DB_URL = f"postgresql://postgres:postgres@localhost:5432/{TEST_DB_NAME}"
+
+def test_db():
+    try:
+        conn = psycopg.connect(TEST_DB_URL, autocommit=True)
+    except psycopg.OperationalError:
+        pytest.fail(f"Could not connect to test database at {TEST_DB_URL}")
+
+    with conn.cursor() as cur:
+        cur.execute("DROP SCHEMA public CASCADE;")
+        cur.execute("CREATE SCHEMA public;")
+    conn.close()
+
+    os.environ["MG_DATABASE_URL"] = TEST_DB_URL
+    yield TEST_DB_URL
+    del os.environ["MG_DATABASE_URL"]
