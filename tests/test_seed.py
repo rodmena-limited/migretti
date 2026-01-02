40efe4f6145205e736ce54b1527aa9533e89ec34
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-01-02 21:36:31 +0000

    Add test_db to test_seed

--- tests/test_seed.py
+++ tests/test_seed.py
@@ -7,3 +7,18 @@ import tempfile
 from migretti import __main__ as main_mod
 TEST_DB_NAME = "migretti_test"
 TEST_DB_URL = f"postgresql://postgres:postgres@localhost:5432/{TEST_DB_NAME}"
+
+def test_db():
+    try:
+        conn = psycopg.connect(TEST_DB_URL, autocommit=True)
+    except psycopg.OperationalError:
+        pytest.fail(f"Could not connect to test database at {TEST_DB_URL}")
+
+    with conn.cursor() as cur:
+        cur.execute("DROP SCHEMA public CASCADE;")
+        cur.execute("CREATE SCHEMA public;")
+    conn.close()
+
+    os.environ["MG_DATABASE_URL"] = TEST_DB_URL
+    yield TEST_DB_URL
+    del os.environ["MG_DATABASE_URL"]
