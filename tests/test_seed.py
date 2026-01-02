fae441cbfe9852b24e1aeb07bf67300053798a6e
Farshid Ashouri <farsheed.ashouri@gmail.com>
2026-01-02 21:24:56 +0000

    Add TEST_DB_URL to test_seed

--- tests/test_seed.py
+++ tests/test_seed.py
@@ -6,3 +6,4 @@ from migretti.logging_setup import setup_logging
 import tempfile
 from migretti import __main__ as main_mod
 TEST_DB_NAME = "migretti_test"
+TEST_DB_URL = f"postgresql://postgres:postgres@localhost:5432/{TEST_DB_NAME}"
